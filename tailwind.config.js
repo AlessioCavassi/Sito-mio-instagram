343a91ef7601d067c4ab5d375fa508f431e609c9
AlessioCavassi <163059423+AlessioCavassi@users.noreply.github.com>
2024-08-24 20:41:09 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false,
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+     content: ['./src/**/*.{js,jsx,ts,tsx}'],
+     theme: {
+       extend: {},
+     },
+     plugins: [],
+   }
